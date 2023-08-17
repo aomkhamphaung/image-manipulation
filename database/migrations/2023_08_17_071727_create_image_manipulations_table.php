53aea6ee8b8496fb347c7adee741437f6dc4a6d8
aomkhamphaung <akhamphaung@gmail.com>
2023-08-17 14:17:37 +0630

    album CRUD

---
+++ database/migrations/2023_08_17_071727_create_image_manipulations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->text('data');
+            $table->string('output_path', 2000)->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+            $table->unsignedBigInteger('album_id')->nullable();
+            $table->foreign('album_id')
+                    ->references('id')
+                    ->on('albums')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
